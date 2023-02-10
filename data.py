8a5245cf21e4f295eb9bc2fdca372e7d30ec1591
HetheAllemand <118325715+HetheAllemand@users.noreply.github.com>
2023-02-09 22:39:20 -0500

    108.3 endpoints

--- data.py
+++ data.py
@@ -10,3 +10,77 @@ me = {
         "zipcode": "12345"
     }
 }
+
+
+mock_catalog = [
+    {
+        "title": "Brownie",
+        "category": "brownie",
+        "price": 3.59,
+        "image": "brownie.jpeg",
+        "_id": "1"
+    },
+    {
+        "title": "Cholcolate Cupcake",
+        "category": "chocolate cupcake",
+        "price": 2.29,
+        "image": "chocolate_cupcake.jpeg",
+        "_id": "2"
+    },
+    {
+        "title": "Cranberry Scone",
+        "category": "craberry scone",
+        "price": 3.49,
+        "image": "cranberry_scone.jpeg",
+        "_id": "3"
+    },
+    {
+        "title": "Croissant",
+        "category": "croissant",
+        "price": 2.13,
+        "image": "croissant.jpeg",
+        "_id": "4"
+    },
+    {
+        "title": "Lemon Cupcake",
+        "category": "lemon cupcake",
+        "price": 2.29,
+        "image": "lemon_cupcake.jpeg",
+        "_id": "5"
+    },
+    {
+        "title": "Rose Cake",
+        "category": "rose cake",
+        "price": 12.49,
+        "image": "rose_cake.webp",
+        "_id": "6"
+    },
+    {
+        "title": "Salt Cookie",
+        "category": "salt cookie",
+        "price": 3.87,
+        "image": "salt_cookie.jpeg",
+        "_id": "7"
+    },
+    {
+        "title": "Tart",
+        "category": "tart",
+        "price": 4.25,
+        "image": "tart.jpeg",
+        "_id": "8"
+    },
+    {
+        "title": "Tartine",
+        "category": "tartine",
+        "price": 1.29,
+        "image": "tartine.jpeg",
+        "_id": "9"
+    },
+    {
+        "title": "Vanilla Bean Cupcake",
+        "category": "vanilla bean cupcake",
+        "price": 2.29,
+        "image": "vanilla_bean_cupcake.jpeg",
+        "_id": "10"
+    }
+]
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from data import me
+from flask import Flask, abort
+from data import me, mock_catalog
 
 import json
 
@@ -34,6 +34,53 @@ def developer_address():
     # return address["street"] + " #" + str(address["number"]) + ", " + address["city"] + ", " + address["zipcode"]
     # f string
     return f'{address["street"]} #{address["number"]}, {address["city"]}, {address["zipcode"]}'
+
+@app.get("/api/catalog")
+def get_catalog():
+    return json.dumps(mock_catalog)
+
+@app.get("/api/catalog/count")
+def count_products():
+    count = len(mock_catalog)
+    return json.dumps(count)
+
+
+@app.get("/api/category/<tart>")
+def prods_by_category(tart):
+    results = []
+    for prod in mock_catalog:
+        if prod["category"] == tart:
+            results.append(prod)
+
+    return json.dumps(results)
+
+
+@app.get("/api/product/<id>")
+def prod_by_id(id):
+    for prod in mock_catalog:
+        if prod["_id"] == id:
+            return json.dumps(prod)
+
+    # not found
+    return abort(404, "Invalid ID")
+
+
+@app.get("/api/product/search/<text>")
+def search_product(text):
+    results = []
+    for prod in mock_catalog:
+        if text.lower() in prod["title"].lower():
+            results.append(prod)
+    
+    return json.dumps(results)
+
+
+@app.get("api/categories")
+def get_categories():
+    results = []
+    for prod in mock_catalog:
+        cat = prod["category"]
+
 
 
 app.run(debug=True)
